7540a53b4f8964cb0935908cdf40d0793973dc8e
MertSubas <mertsubas@gmail.com>
2024-09-21 02:14:06 +0300

    Google Maps added

--- Dugundeyiz/Controllers/ServiceController.cs
+++ Dugundeyiz/Controllers/ServiceController.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dugundeyiz.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dugundeyiz.Controllers
 {
     public class ServiceController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<HomeController> _logger;
+        private readonly DugundeyizContext _context;
+
+
+        public ServiceController(ILogger<HomeController> logger, DugundeyizContext context)
         {
+            _logger = logger;
+            _context = context;
+        }
+
+        [Route("Hizmetler/{categoryName}")]
+        public IActionResult Index(string categoryName)
+        {
+            var category = _context.Categories.Where(x => x.CategoryName == categoryName && x.Deleted != true).FirstOrDefault();
             return View();
         }
     }
