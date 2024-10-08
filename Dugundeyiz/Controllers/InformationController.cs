82b04867a2401111b3aa1d9ca54fef8711ce8803
MertSubas <mertsubas@gmail.com>
2024-10-08 15:09:09 +0300

    Information added

---
+++ Dugundeyiz/Controllers/InformationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dugundeyiz.Controllers
+{
+    [Route("/Hakkimizda")]
+    public class InformationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
